49ef2b593848bc2ed2a309c9ef84b9573a4f62f6
Mohan Kumar <46491614+imshines@users.noreply.github.com>
2020-04-21 11:38:59 +0530

    filler pages added

--- src/app/hospitals/hospitals.page.ts
+++ src/app/hospitals/hospitals.page.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var google;
+
 @Component({
   selector: 'app-hospitals',
   templateUrl: './hospitals.page.html',
   styleUrls: ['./hospitals.page.scss'],
 })
+
 export class HospitalsPage implements OnInit {
+  
+  map: any;
 
   constructor() { }
-
+  
+  ionViewDidEnter(){
+    //Set latitude and longitude of some place
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: -34.9011, lng: -56.1645 },
+      zoom: 15
+    });
+  }
   ngOnInit() {
   }
 
--- src/app/medshops/medshops.page.ts
+++ src/app/medshops/medshops.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var google;
+
 @Component({
   selector: 'app-medshops',
   templateUrl: './medshops.page.html',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedshopsPage implements OnInit {
 
+  map: any;
+
   constructor() { }
+
+  ionViewDidEnter(){
+    //Set latitude and longitude of some place
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: -34.9011, lng: -56.1645 },
+      zoom: 15
+    });
+  }
 
   ngOnInit() {
   }
