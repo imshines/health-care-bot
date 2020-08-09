47afbd54ebd5160a40f5af305abaae8a41f08ed9
imshines <shinesemm@gmail.com>
2020-08-09 14:49:51 +0530

    login added

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { AuthService } from '../services/auth.service';
 import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { async } from '@angular/core/testing';
-=======
->>>>>>> 71a8cd4605c2ef06b4d68542a1bb56587a97e07d
 
 @Component({
   selector: 'app-login',
@@ -15,17 +12,12 @@ import { async } from '@angular/core/testing';
 })
 export class LoginPage implements OnInit {
 
-<<<<<<< HEAD
   constructor(private authService:AuthService,private router:Router,
     private alertCtrl:AlertController) { }
-=======
-  constructor() { }
->>>>>>> 71a8cd4605c2ef06b4d68542a1bb56587a97e07d
 
   ngOnInit() {
   }
 
-<<<<<<< HEAD
   async loginUser(form):Promise<void>{
     this.authService.loginUser(form.value.email, form.value.password).then(
       ()=>{
@@ -46,6 +38,4 @@ export class LoginPage implements OnInit {
     this.router.navigateByUrl('signup');
   }
 
-=======
->>>>>>> 71a8cd4605c2ef06b4d68542a1bb56587a97e07d
 }
